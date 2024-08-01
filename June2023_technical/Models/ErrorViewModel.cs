57365c9056adf8b9a6cd7ae405cb22571ecfd2ea
GrYzLy <bartosz.rzemek@gmail.com>
2024-08-01 21:31:18 +0200

    First commit

---
+++ June2023_technical/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace June2023_technical.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+

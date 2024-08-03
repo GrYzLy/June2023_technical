ebf8cef8c40c81e928cfead9d9d60f711c27d205
KrzysztofBromCodeCool <krzysztofbrom@gmail.com>
2024-08-03 11:21:40 +0200

    BooksController, Model, Views corrections 2

---
+++ June2023_technical/Models/BookContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace June2023_technical.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- June2023_technical/Program.cs
+++ June2023_technical/Program.cs
@@ -1,7 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using June2023_technical.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add database context
+builder.Services.AddDbContext<BookContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -9,7 +16,9 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -21,4 +30,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-

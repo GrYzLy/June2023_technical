9a08b10e343895e3a3256aeaa01dc3f56180adeb
Szymon Kałkowski <136834181+KalkowskiSzymon@users.noreply.github.com>
2024-08-03 10:32:27 +0200

    commit
    10:32

---
+++ June2023_technical/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public int PublicationYear { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
